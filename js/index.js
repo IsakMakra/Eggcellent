082f3e38db3efdad9cced07b291f161f0e8555f6
Ossian Dahlberg <ossiandahlberg@hotmail.com>
2023-10-02 15:15:56 +0200

    s

--- js/index.js
+++ js/index.js
@@ -1,46 +1,4 @@
 "use strict";
 
-document.getElementById("info_button").addEventListener("click", display_info);
-
-function display_info () {
-    document.querySelector("#wrapper").innerHTML += `
-        <div id="overlay"> 
-            <div id="info_background">
-                <div id="close_button"></div>
-                <p id="info_text"></p>
-                <div id="next_message_button"></div>
-            </div>
-        </div>
-    `;
-
-    let nr_of_messages_displayed = 0;
-
-    let messages = [
-        `Click on "customize" button at the buttom of the screen to manage your options.`,
-        `Before putting your eggs in the pot, make sure that the water is boiling.`,
-        `Poke a hole in the bottom of the egg/eggs to minimize the risk of the egg cracking.`,
-        `Gently lower the egg/eggs into the pot, then start the timer!`
-    ];
-
-    let info_text = document.getElementById("info_text");
-    info_text.textContent = messages[0];
-
-    let next_message_button = document.getElementById("next_message_button");
-    next_message_button.addEventListener("click", display_next_message);
-
-    document.getElementById("close_button").addEventListener("click", () => {
-        document.getElementById("overlay").remove();
-        document.querySelector("#info_button").addEventListener("click", display_info);
-    })
-
-    function display_next_message() {
-        nr_of_messages_displayed += 1;
-
-        info_text.textContent = messages[nr_of_messages_displayed];
-
-        if(nr_of_messages_displayed === messages.length - 1) {
-            next_message_button.remove();
-        }
-    }
-}
+initiateMainPage();
 
--- js/mainPage.js
+++ js/mainPage.js
@@ -1,36 +1,110 @@
 "use strict";
 
+function initiateMainPage(){
+    // document.querySelector("#startTimer").addEventListener("click", startTimer);
+    document.querySelector("#customizeEgg").addEventListener("click", renderCustomizeModal);
+    document.getElementById("info_button").addEventListener("click", display_info);
+}
+
 function renderCustomizeModal(){
 
     const customizeModal = document.createElement("div");
-    customizeModal.classList.add("modal");
+    customizeModal.setAttribute("id", "overlay");
     customizeModal.innerHTML = `
-        <div class="modalContainer">
+        <div id="customizePage">
+
             <div id="eggsCount">
-                <div id="countMinus">-</div>
-                <div id="eggCounter"></div>
-                <div id="countPlus">+</div>
+                <div>How many eggs?</div>
+                <div class="options">
+                    <div id="countMinus">-</div>
+                    <div id="eggCounter">4</div>
+                    <div id="countPlus">+</div>
+                </div>
             </div>
+
             <div id="coldOrWarm">
-                <div id="cold"></div>
-                <div id="warm"></div>
+                <div>Are they cold or warm?</div>
+                <div class="options">
+                    <div id="cold"></div>
+                    <div id="warm"></div>
+                </div>
             </div>
+
             <div id="eggSize">
-                <div id="smallEgg"></div>
-                <div id="mediumEgg"></div>
-                <div id="largeEgg"></div>
-                <div id="extraLargeEgg"></div>
+                <div>Egg size</div>
+                <div class="options">
+                    <div id="smallEgg">S</div>
+                    <div id="mediumEgg">M</div>
+                    <div id="largeEgg">L</div>
+                    <div id="extraLargeEgg">XL</div>
+                </div>
             </div>
+
             <div id="boilType">
-                <div id="softBoiled"></div>
-                <div id="mediumBoiled"></div>
-                <div id="hardBoiled"></div>
+                <div>Boil-type</div>
+                <div class="options">
+                    <div id="softBoiled">
+                        <div id="soft"></div>
+                        Soft
+                    </div>
+                    <div id="mediumBoiled">
+                        <div id="medium"></div>
+                        Medium
+                    </div>
+                    <div id="hardBoiled">
+                        <div id="hard"></div>
+                        Hard
+                    </div>        
+                </div>
             </div>
-            <div id="averageTime"></div>
-            <button id="closeCustomize"></button>
+
+            <div id="averageTime">04:37</div>
+            <button id="closeCustomize">Close</button>
         </div>
     `
     
     document.querySelector("#wrapper").appendChild(customizeModal);
     
+}
+
+function display_info () {
+    document.querySelector("#wrapper").innerHTML += `
+        <div id="overlay"> 
+            <div id="info_background">
+                <div id="close_button"></div>
+                <p id="info_text"></p>
+                <div id="next_message_button"></div>
+            </div>
+        </div>
+    `;
+
+    let nr_of_messages_displayed = 0;
+
+    let messages = [
+        `Click on "customize" button at the buttom of the screen to manage your options.`,
+        `Before putting your eggs in the pot, make sure that the water is boiling.`,
+        `Poke a hole in the bottom of the egg/eggs to minimize the risk of the egg cracking.`,
+        `Gently lower the egg/eggs into the pot, then start the timer!`
+    ];
+
+    let info_text = document.getElementById("info_text");
+    info_text.textContent = messages[0];
+
+    let next_message_button = document.getElementById("next_message_button");
+    next_message_button.addEventListener("click", display_next_message);
+
+    document.getElementById("close_button").addEventListener("click", () => {
+        document.getElementById("overlay").remove();
+        document.querySelector("#info_button").addEventListener("click", display_info);
+    })
+
+    function display_next_message() {
+        nr_of_messages_displayed += 1;
+
+        info_text.textContent = messages[nr_of_messages_displayed];
+
+        if(nr_of_messages_displayed === messages.length - 1) {
+            next_message_button.remove();
+        }
+    }
 }
