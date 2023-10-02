48884e8b89f86d7f7d1f3e397018a4f92a09a6f3
Isak Makra <89833141+IsakMakra@users.noreply.github.com>
2023-10-02 15:08:27 +0200

    Made some changes to info display

--- js/index.js
+++ js/index.js
@@ -1,19 +1,19 @@
 "use strict";
 
+document.getElementById("info_button").addEventListener("click", display_info);
+
 function display_info () {
-    document.querySelector("wrapper").innerHTML += `
+    document.querySelector("#wrapper").innerHTML += `
         <div id="overlay"> 
             <div id="info_background">
-                <p id="number"></p>
-                <p id="info_text"><p>
-                <p id="next_message_button">Next<p>
-                <p id="close_button">Close<p>
-            <div>
-        <div>
+                <div id="close_button"></div>
+                <p id="info_text"></p>
+                <div id="next_message_button"></div>
+            </div>
+        </div>
     `;
 
     let nr_of_messages_displayed = 0;
-    let current_number = 1;
 
     let messages = [
         `Click on "customize" button at the buttom of the screen to manage your options.`,
@@ -22,8 +22,6 @@ function display_info () {
         `Gently lower the egg/eggs into the pot, then start the timer!`
     ];
 
-    let number = document.getElementById("number");
-    number.textContent = `${current_number}.`
     let info_text = document.getElementById("info_text");
     info_text.textContent = messages[0];
 
@@ -32,15 +30,13 @@ function display_info () {
 
     document.getElementById("close_button").addEventListener("click", () => {
         document.getElementById("overlay").remove();
-        document.querySelector("button").addEventListener("click", display_info);
+        document.querySelector("#info_button").addEventListener("click", display_info);
     })
 
     function display_next_message() {
         nr_of_messages_displayed += 1;
-        current_number += 1;
 
         info_text.textContent = messages[nr_of_messages_displayed];
-        number.textContent = `${current_number}.`
 
         if(nr_of_messages_displayed === messages.length - 1) {
             next_message_button.remove();
